096350c3a71299b6609ee76f5ad61e14c2138963
RazorHollow <rob@razorhollow.com>
2022-06-15 18:21:57 -0400

    initial commit

---
+++ data/skill-data.js
@@ -0,0 +1,32 @@
+let skills = [
+  {
+    name: 'html',
+    url: "https://html.spec.whatwg.org/",
+    falogo: "<i class='fa-brands fa-html5'></i>",
+  },
+  {
+    name: 'css',
+    url: "https://www.w3.org/Style/CSS/Overview.en.html",
+    falogo: '<i class="fa-brands fa-css3"></i>'
+  },
+  {
+    name: 'javascript',
+    url: 'https://www.ecma-international.org/publications-and-standards/standards/ecma-262/',
+    falogo: '<i class="fa-brands fa-js"></i>'
+  },
+  {
+    name: 'express',
+    url: 'http://expressjs.com/',
+    falogo: ''
+  },
+  {
+    name: 'mongoose',
+    url: 'https://mongoosejs.com',
+    falogo: ''
+  },
+  {
+    name: 'mongo db',
+    url: 'http://mongodb.com',
+    falogo: '<i class="fa-solid fa-database"></i>'
+  }
+]
