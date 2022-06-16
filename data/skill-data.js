fa37e02467b4ed190f1be943c649c81f5bdf61ab
RazorHollow <rob@razorhollow.com>
2022-06-15 21:59:52 -0400

    add bootstrap styling

--- data/skill-data.js
+++ data/skill-data.js
@@ -2,32 +2,32 @@ let skills = [
   {
     name: 'html',
     url: "https://html.spec.whatwg.org/",
-    falogo: "<i class='fa-brands fa-html5'></i>",
+    falogo: 'fa-brands fa-html5',
   },
   {
     name: 'css',
     url: "https://www.w3.org/Style/CSS/Overview.en.html",
-    falogo: '<i class="fa-brands fa-css3"></i>'
+    falogo: 'fa-brands fa-css3'
   },
   {
     name: 'javascript',
     url: 'https://www.ecma-international.org/publications-and-standards/standards/ecma-262/',
-    falogo: '<i class="fa-brands fa-js"></i>'
+    falogo: 'fa-brands fa-js'
   },
   {
     name: 'express',
     url: 'http://expressjs.com/',
-    falogo: ''
+    falogo: "fa-solid fa-e"
   },
   {
     name: 'mongoose',
     url: 'https://mongoosejs.com',
-    falogo: ''
+    falogo: 'fa-solid fa-squirrel'
   },
   {
     name: 'mongo db',
     url: 'http://mongodb.com',
-    falogo: '<i class="fa-solid fa-database"></i>'
+    falogo: 'fa-solid fa-database'
   }
 ]
 
